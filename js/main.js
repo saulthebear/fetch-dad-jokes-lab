1b5366db9aab30a7f9868da2df56514fb70b8bac
Stefan Vosloo <saulthebear@users.noreply.github.com>
2022-05-17 17:19:04 -0600

    Refactor fetch headers

--- js/main.js
+++ js/main.js
@@ -4,20 +4,16 @@ const displayElement = document.querySelector("#joke-display")
 const imageContainer = document.querySelector("#img-container")
 const imageElement = document.querySelector("#joke-img")
 
-// Set headers for request
-const headers = new Headers()
-headers.append("Accept", "application/json")
-
 // base url to fetch from
 const url = "https://icanhazdadjoke.com/"
-// Create a request object, containing our headers
-const request = new Request(url, {
-  headers: headers,
-})
 
 // Fetch joke and image
 const fetchJoke = () => {
-  fetch(request)
+  fetch(url, {
+    headers: {
+      Accept: "application/json",
+    },
+  })
     .then((response) => response.json()) // transform to json
     .then((json) => {
       displayElement.innerText = json.joke // get joke text and set DOM
@@ -29,9 +25,7 @@ const fetchJoke = () => {
         .then((response) => response.blob()) // transform response to blob
         .then((blob) => {
           const objectURL = URL.createObjectURL(blob) // transform blob to url
-          // const imageElement = document.createElement("img") // create an HTML img element
           imageElement.src = objectURL // use object url we created as img src
-          // imageContainer.appendChild(imageElement) // add the HTML img element to the DOM
         })
     })
     .catch((error) => console.warn("something went wrong", error))
